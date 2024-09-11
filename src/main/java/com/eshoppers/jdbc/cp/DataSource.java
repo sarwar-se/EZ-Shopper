0db387dbdf2dd1f6efa66435ec28b89d63016ecb
sarwar71 <mh.sarwar298@gmail.com>
2024-09-11 09:55:31 +0600

    Learn jdbc api & connection pool.

---
+++ src/main/java/com/eshoppers/jdbc/cp/DataSource.java
@@ -0,0 +1,13 @@
+package com.eshoppers.jdbc.cp;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class DataSource {
+    private static JdbcConnectionPool pool = new JdbcConnectionPool();
+
+    public static Connection getConnection() throws SQLException {
+
+        return pool.getConnectionFromPool();
+    }
+}
